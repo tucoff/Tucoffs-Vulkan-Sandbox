edebf3b81d7d8690de17f2212ecc4c4c9100a6f0
tucoff <baracho.arturraffael@gmail.com>
2025-06-20 22:23:41 -0300

    organization changes

---
+++ triangle/compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/bin/glslc shader.vert -o vert.spv
+/usr/bin/glslc shader.frag -o frag.spv
---
+++ triangle/epic_triangle.h
@@ -0,0 +1,7 @@
+// epic_triangle.h
+#ifndef EPIC_TRIANGLE_H
+#define EPIC_TRIANGLE_H
+
+int triangle(); // Apenas a declaração
+
+#endif
